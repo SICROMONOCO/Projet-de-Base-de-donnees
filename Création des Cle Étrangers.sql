5c19cf4e42d2f6263f589d4473975ac4cf2c323e
BILAL SIKI <bilal06siki@gmail.com>
2025-03-02 15:43:56 +0000

    Update Création des Cle Étrangers.sql

--- Création des Cle Étrangers.sql
+++ Création des Cle Étrangers.sql
@@ -7,11 +7,6 @@ ALTER TABLE Etudiants
 ADD CONSTRAINT fk_etudiants_classe
 FOREIGN KEY (classe_id) REFERENCES Classe(classe_id); -- Lie chaque étudiant à sa classe id
 
--- Ajouter les clés étrangères à la table Classe
-ALTER TABLE Classe
-ADD CONSTRAINT fk_classe_matieres
-FOREIGN KEY (matières_id) REFERENCES Matières(matières_id); -- Associe chaque classe à une matière via son id
-
 -- Ajouter les clés étrangères à la table Professeurs 
 ALTER TABLE Professeurs
 ADD CONSTRAINT fk_professeurs_matieres
@@ -25,9 +20,13 @@ FOREIGN KEY (professeur_id) REFERENCES Professeurs(professeur_id); -- Relie chaq
 -- Ajouter les clés étrangères à la table Examens
 ALTER TABLE Examens
 ADD CONSTRAINT fk_examens_classe
-FOREIGN KEY (classe_id) REFERENCES Classe(classe_id); -- Relie chaque examen à la classe qui le passe
+FOREIGN KEY (classe_id) REFERENCES Classe(classe_id), -- Relie chaque examen à la classe des étudiants 
+ADD CONSTRAINT fk_examens_matières
+FOREIGN KEY (matières_id) REFERENCES Matières(matières_id); -- Relie chaque examen à la matière qu'il enseigne par id
 
 -- Ajouter les clés étrangères à la table Notes
 ALTER TABLE Notes
+ADD CONSTRAINT fk_notes_examens
+FOREIGN KEY (examen_id) REFERENCES Examens(examen_id), -- Relie chaque examen à la examens par id
 ADD CONSTRAINT fk_notes_etudiants
-FOREIGN KEY (étudiants_id) REFERENCES Etudiants(étudiants_id) ON DELETE CASCADE, -- Lie chaque note à un étudiant par id
+FOREIGN KEY (étudiants_id) REFERENCES Etudiants(étudiants_id) ON DELETE CASCADE; -- Lie chaque note à un étudiant par id
