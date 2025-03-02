8ea346e6120db17f2ee3eb617e9beb28658462a5
BILAL SIKI <bilal06siki@gmail.com>
2025-03-02 15:26:12 +0000

    Update Création de Base de données et les Tableaux.sql

--- Création de Base de données et les Tableaux.sql
+++ Création de Base de données et les Tableaux.sql
@@ -1,30 +1,25 @@
 /* Query [Server] */
 
-CREATE DATABASE EducationLDB;
-USE EducationLDB;
+CREATE DATABASE EducationLDB2;
+USE EducationLDB2;
 
 /* Query [educationldb] */
 
 CREATE TABLE Classe (
   classe_id INT PRIMARY KEY AUTO_INCREMENT,
-  classe_name VARCHAR(50) NOT NULL,
-  Niveaux INT CHECK (
-    Niveaux BETWEEN 1
-    AND 12
-  ),
-  matières_code VARCHAR(10) UNIQUE
+  classe_nom VARCHAR(50) NOT NULL,
 );
 
 CREATE TABLE Etudiants (
   étudiants_id INT PRIMARY KEY AUTO_INCREMENT,
-  classe_id INT NOT NULL,
+  classe_id INT NULL,
   personnel_nom VARCHAR(50) NOT NULL,
   famille_nom VARCHAR(50) NOT NULL,
   date_de_naissance DATE NOT NULL,
   sexe ENUM('Male', 'Female', 'Other'),
   email VARCHAR(100) UNIQUE,
   phone VARCHAR(20),
-  adresse TEXT
+  adresse TEXT NULL
 );
 
 CREATE TABLE Matières (
@@ -46,6 +41,7 @@ CREATE TABLE Professeurs (
 CREATE TABLE Examens (
   Examen_id INT PRIMARY KEY AUTO_INCREMENT,
   classe_id INT,
+  matières_id INT,
   Examen_nom VARCHAR(50) NOT NULL,
   Examen_date DATE NOT NULL
 );
@@ -53,6 +49,7 @@ CREATE TABLE Examens (
 CREATE TABLE Notes (
   notes_id INT PRIMARY KEY AUTO_INCREMENT,
   étudiants_id INT,
+  Examen_id INT,
   Note_obtenue FLOAT CHECK ( Note_obtenue BETWEEN 0 AND 20 ),
   Mention ENUM('Passable', 'Assez bien', 'Bien', 'Très bien'),
   Commentaire TEXT
